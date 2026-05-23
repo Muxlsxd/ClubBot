326bb75e4a41d5dbe60720eb2bc21b632e5cd3f8
Muxlsxd <muazzinnimah@gmail.com>
2026-05-23 08:17:00 +0700

    debug webhook

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-require("dotenv").config();
-
 const express = require("express");
-const line = require("@line/bot-sdk");
 
 const app = express();
 
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
-};
-
-const client = new line.messagingApi.MessagingApiClient({
-  channelAccessToken: config.channelAccessToken
-});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Club Bot Online");
 });
 
-app.post(
-  "/webhook",
-  express.json(),
-  (req, res) => {
+app.post("/webhook", (req, res) => {
 
-    console.log("===== WEBHOOK =====");
-    console.log(JSON.stringify(req.body, null, 2));
+  console.log("===== WEBHOOK RECEIVED =====");
+  console.log(JSON.stringify(req.body, null, 2));
 
-    res.sendStatus(200);
-  }
-);
+  res.status(200).send("OK");
+});
 
 const PORT = process.env.PORT || 3000;
 
